a57b800bdb6f03c011ca4c37706b8349153eb53f
linhnguyen <quang@gmail.com>
2019-09-11 16:43:28 +0700

    tam xong

---
+++ MultiThread/MultiThread/BangChiaLuong.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MultiThread
+{
+    public class BangChiaLuong
+    {
+        private Panel panelBoardControls;
+        private Panel panelBoardThread;
+
+        public BangChiaLuong() { }
+
+        public BangChiaLuong(Panel panelBoardControls, Panel panelBoardThread) : this()
+        {
+            this.panelBoardControls = panelBoardControls;
+            this.panelBoardThread = panelBoardThread;
+        }
+
+        public List<Button> DsButton { get; set; }
+        public List<Thread> DsThread { get; set; }
+
+        void TaobangLuong(int soLuong)
+        {
+            int ox = panelBoardControls.Location.X;
+            panelBoardThread.Controls.Clear();
+            this.DsButton = new List<Button>();
+            DsThread = new List<Thread>();
+            int x = panelBoardThread.Width;
+            int oy = panelBoardThread.Location.Y;
+            int y = panelBoardThread.Height;
+            int w = (x - ox) / 2;
+            int h = (y - oy) / (soLuong / 2);
+            int dem = 1;
+            for (int i = 0; i < soLuong / 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    var button = new Button()
+                    {
+                        Width = w,
+                        Height = h,
+                        Location = new Point(ox, oy),
+                        Text = "luồng " + dem.ToString(),
+                    };
+
+                    DsButton.Add(button);
+                    ox += w;
+                    dem++;
+                }
+                oy += h;
+                ox = panelBoardControls.Location.X;
+            }
+            Update();
+        }
+        void Update()
+        {
+            foreach (var button in DsButton)
+            {
+                panelBoardThread.Controls.Add(button);
+            }
+        }
+        public void Start(long a, long b, int soLuong)
+        {
+            TaobangLuong(soLuong);
+            int dem = 1;
+            long i = a;
+            long bacNhay = (b - a) / soLuong;
+            while (true)
+            {
+                if (b - i > bacNhay)
+                {
+                    var x = new Thread((new SoNguyenTo(i, i + bacNhay).LaySNT));
+                    x.Name = dem.ToString();
+                    x.Start();
+                    DsThread.Add(x);
+                }
+                else
+                {
+                    var x = new Thread((new SoNguyenTo(i, b).LaySNT));
+                    x.Name = i.ToString();
+                    x.Start();
+                    DsThread.Add(x);
+                    break;
+                }
+                i = i + bacNhay;
+                dem++;
+            }
+        }
+
+        public void KiemTraLuongChay(int second)
+        {
+            for (int i = 0; i < DsThread.Count; i++)
+            {
+                if (DsThread[i].Join(second))
+                    DsButton[i].Text ="da xong";
+                else DsButton[i].Text = "chua xong";
+            }
+            Update();
+        }
+    }
+}
---
+++ MultiThread/MultiThread/SoNguyenTo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThread
+{
+    public class SoNguyenTo
+    {
+        public SoNguyenTo(long bDau, long kThuc)
+        {
+            this.BatDau = bDau;
+            this.KetThuc = kThuc;
+        }
+
+        public long BatDau { get; private set; }
+        public long KetThuc { get; private set; }
+
+        public static List<long> DsSNT = new List<long>();
+
+        public override string ToString()
+        {
+            string s = DsSNT.Count.ToString() + "\n";
+            foreach (var item in DsSNT)
+            {
+                s += item.ToString() + "\t";
+            }
+            return s;
+        }
+        public void LaySNT()
+        {
+            for (long i = BatDau; i < KetThuc; i++)
+            {
+                if (KiemTraSNT(i))
+                {
+                    DsSNT.Add(i);
+                }
+            }
+        }
+        public bool KiemTraSNT(long a)
+        {
+            if (a % 2 == 0)
+                return false;
+            else
+            {
+                for (long i = 2; i <= Math.Sqrt(a); i++)
+                    if (a % i == 0)
+                        return false;
+                if (a < 2)
+                    return false;
+                else if (a == 2)
+                    return true;
+                return true;
+            }
+        }
+    }
+}
